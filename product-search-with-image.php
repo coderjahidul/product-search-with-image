51aa54ac608671f9cb8bcf17342d1e4d06be4f5b
coderjahidul <sobuz0349@gmail.com>
2025-08-09 14:44:37 +0600

    add plugin all file

---
+++ product-search-with-image.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * WooCommerce Product Search with Image
+ * Description: Adds AJAX-powered WooCommerce product search that displays results with product images.
+ * Version: 1.0
+ * Author: MD Jahidul Islam
+ * Author URI: https://mdjahidulislam.com
+ */
+?>
